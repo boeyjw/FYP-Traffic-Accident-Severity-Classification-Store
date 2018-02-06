ea9b46f0164353d601b1202edf10e94f3af957af
boeyjw <boey_secreto@hotmail.com>
2018-02-06 21:05:57 +0000

    nn model in progress

--- nn.py
+++ nn.py
@@ -6,38 +6,32 @@ where all it predicts is class 3 (majority class)
 import pandas as pd
 import numpy as np
 
-from sklearn.preprocessing import LabelBinarizer
-from sklearn.metrics import accuracy_score, classification_report
+from tap import tap
 from sklearn.model_selection import train_test_split
 from sklearn.externals import joblib
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
-from keras.utils import to_categorical
 from keras.callbacks import TerminateOnNaN, EarlyStopping, TensorBoard, CSVLogger
 
 print('Init')
-RANDOM_STATE = 123456789
-BATCH_SIZE = 2048000
-tap = pd.read_csv('Imputation/2.3/acc2005_2016-v2018.2.3.imp.csv').merge(pd.read_csv('Imputation/2.3/veh2005_2016-v2018.2.3.imp.csv'), on = 'Accident_Index', how = 'inner').drop(['Accident_Index', 'Date_Time'], axis = 1)
-sample_size = sum(tap.loc[tap['Accident_Severity'] == 1, 'Accident_Severity'])
-
-X_train, X_test, Y_train, Y_test = train_test_split(tap.drop('Accident_Severity', axis=1), to_categorical(tap['Accident_Severity']), test_size = 0.3, random_state = RANDOM_STATE)
-
-input_dim = len(tap.columns.drop('Accident_Severity'))
-
-print('Fit')
-model = Sequential()
-model.add(Dense(input_dim * 2, input_dim = input_dim, activation='sigmoid'))
-model.add(Dense(input_dim, activation='sigmoid'))
-model.add(Dense(4, activation='softmax'))
-model.compile(optimizer = 'sgd', loss = 'categorical_crossentropy', metrics=['categorical_accuracy'])
-
-callbacks = [
+tap = tap(ver_dir = '2.3')
+data = tap.load_tap(binarizer_mode = 'keras')
+params = tap.model_general_parameters(data_len = len(data.x), optimizer = 'sgd', loss = 'categorical_crossentropy', metrics = ['categorical_accuracy'], callbacks = [
                 TerminateOnNaN(),
                 EarlyStopping(patience=10),
                 CSVLogger('nn-hist-v1.csv')
-            ]
-hist = model.fit(X_train, Y_train, batch_size = BATCH_SIZE, epochs = 30, validation_split = 0.1, callbacks = callbacks)
+            ])
+
+X_train, X_test, Y_train, Y_test = train_test_split(data.x, data.y, test_size = params['test_size'], random_state = params['random_state'])
+
+print('Fit')
+model = Sequential()
+model.add(Dense(len(tap.x_cols) * 2, input_dim = len(tap.x_cols), activation='elu'))
+model.add(Dense(len(tap.x_cols), activation='elu'))
+model.add(Dense(4, activation='softmax'))
+model.compile(optimizer = params['optimizer'], loss = params['loss'], metrics = params['metrics'])
+
+hist = model.fit(X_train, Y_train, batch_size = params['batch_size'], epochs = params['epochs'], validation_split = params['validation_split'], callbacks = params['callbacks'])
 model.save('nn_model-v1.h5')
 
 print('Predict')
--- tap.py
+++ tap.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 from collections import namedtuple
 from sklearn.preprocessing import LabelBinarizer
@@ -6,18 +7,48 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
 from keras.utils import to_categorical
 class tap:
-    def load_tap(self, ver_dir, ver_file = None, binarizer_mode = 'sklearn', use_current_dir = False):
+    __data = pd.DataFrame()
+    x_cols = None
+    rec_len = None
+    """
+    Traffic Accident Prediction data and hyperparameter manager
+    """
+    def __init__(self, ver_dir, ver_file = None, use_current_dir = False):
+        # Get the directory right
         if ver_file is None:
             ver_file = ver_dir
         directory = './' if use_current_dir == True else 'Imputation/' + ver_dir + '/'
-        tap = pd.read_csv(directory + 'acc2005_2016-v2018.' + ver_file + '.imp.csv').merge(pd.read_csv(directory + 'veh2005_2016-v2018.' + ver_file + '.imp.csv'), on = 'Accident_Index', how = 'inner').drop(['Accident_Index', 'Date_Time'], axis = 1)
+        # Import CSV data
+        self.__data = pd.read_csv(directory + 'acc2005_2016-v2018.' + ver_file + '.imp.csv').merge(pd.read_csv(directory + 'veh2005_2016-v2018.' + ver_file + '.imp.csv'), on = 'Accident_Index', how = 'inner').drop(['Accident_Index', 'Date_Time'], axis = 1)
+        # Initialise general pointers on the dataset
+        self.x_cols = self.__data.columns.drop('Accident_Severity')
+        self.rec_len = len(self.__data)
 
-        Data = namedtuple('Data', ['x', 'y'])
-        return Data(tap.drop('Accident_Severity', axis=1), LabelBinarizer().fit_transform(tap['Accident_Severity']) if binarizer_mode == 'sklearn' else to_categorical(tap['Accident_Severity']))
+    def load_tap(self, binarizer_mode = 'sklearn'):
+        data = namedtuple('data', ['x', 'y'])
+        return data(self.__data.drop('Accident_Severity', axis=1),
+        LabelBinarizer().fit_transform(self.__data['Accident_Severity']) if binarizer_mode == 'sklearn' else to_categorical(self.__data['Accident_Severity']))
 
-    def model_general_parameters(self, random_state = 500, batch_size = 512000, n_jobs = 6, learning_rate = 0.01, epochs = 150, test_size = 0.3, validation_split = 0.1, **kwargs):
-        pass
+    def model_general_parameters(self, data_len, random_state = 500, batch_size = 0.1, n_jobs = 6, learning_rate = 0.01, epochs = 150, test_size = 0.3, validation_split = 0.1, **kwargs):
+        params = {
+            'random_state': random_state,
+            'batch_size': int(np.ceil(data_len * (1 - test_size) * batch_size)) if isinstance(batch_size, float) else batch_size,
+            'n_jobs': n_jobs,
+            'learning_rate': learning_rate,
+            'epochs': epochs,
+            'test_size': test_size,
+            'validation_split': validation_split
+        }
+        for k, v in kwargs.items():
+            params[k] = v
+
+        return params
 
     def evaluate_model(self, y_true, y_pred, mode = 'weighted'):
-        pass
+        score = {
+            'acc': accuracy_score(y_true, y_pred),
+            'cm': confusion_matrix(y_true, y_pred),
+            'report': classification_report(y_true, y_pred)
+        }
+        return score
 
