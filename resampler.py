377f4ce60fcc5ab8e4782ca2fbfa5dd9a292c36f
boeyjw <boey_secreto@hotmail.com>
2018-02-20 18:28:06 +0000

    Multiprocessing sampling, currently in test bed

--- resampler.py
+++ resampler.py
@@ -1,5 +1,6 @@
+from multiprocessing import Pool, Process, Lock, Manager
+
 from tap import modelling, modelmetrics
-from collections import OrderedDict
 
 from imblearn.under_sampling import TomekLinks
 from imblearn.over_sampling import SMOTE
@@ -9,48 +10,51 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.externals import joblib
 
-model = modelling('2.5')
-data = model.load_tap()
-params = model.model_general_parameters(n_jobs=6, data_len=len(data.y))
-
-metrics = modelmetrics()
-res = OrderedDict([(clf, {}) for clf in ['default', 'tomek', 'smote', 'smotetomek']])
-
-def rf_check_perf(x, y):
+def rf_check_perf(x, y, name=None):
     rf = RandomForestClassifier(n_estimators=30, n_jobs=params['n_jobs'], random_state=params['random_state'])
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=params['test_size'], random_state=params['random_state'])
     rf.fit(x_train, y_train)
     y_pred = rf.predict(x_test)
-    return metrics.evaluate_model(y_pred=y_pred, y_true=y_test)
+    return metrics.evaluate_model(y_pred=y_pred, y_true=y_test, name=name, do_print=True)
 
 def joblib_dump(x, y, postfix):
     joblib.dump(x, 'tmp/' + 'X_' + postfix + '.pkl')
     joblib.dump(y, 'tmp/' + 'Y_' + postfix + '.pkl')
 
+def resample(sampler, name, datax, datay, res, lock):
+    print(name)
+    x_res, y_res = sampler.fit_sample(datax, datay)
+    lock.acquire()
+    res[name] = rf_check_perf(x_res, y_res, name=name)
+    lock.release()
+    joblib_dump(x_res, y_res, name)
+
+model = modelling('2.5')
+data = model.load_tap()
+params = model.model_general_parameters(n_jobs=6, data_len=len(data.y))
+
+metrics = modelmetrics()
+res = Manager().dict([(clf, {}) for clf in ['default', 'tomek', 'smote', 'smotetomek']])
+
+samplers = {
+    'tomek': TomekLinks(n_jobs=params['n_jobs'], random_state=params['random_state']),
+    'smote': SMOTE(n_jobs=params['n_jobs'], random_state=params['random_state'], kind='borderline1')
+}
+
+samplers['smotetomek'] = SMOTETomek(random_state=params['random_state'], smote=samplers['smote'], tomek=samplers['tomek'])
+
 print('No sampling')
 res['default'] = rf_check_perf(data.x, data.y)
 
-print('Tomek')
-tomek = TomekLinks(n_jobs=params['n_jobs'], random_state=params['random_state'])
-x_res, y_res = tomek.fit_sample(data.x, data.y)
-res['tomek'] = rf_check_perf(x_res, y_res)
-joblib_dump(x_res, y_res, 'tomek')
-del x_res, y_res
-
-print('SMOTE')
-smote = SMOTE(n_jobs=params['n_jobs'], random_state=params['random_state'], kind='borderline1')
-x_res, y_res = smote.fit_sample(data.x, data.y)
-res['smote'] = rf_check_perf(x_res, y_res)
-joblib_dump(x_res, y_res, 'smote')
-del x_res, y_res
-joblib.dump(res, 'tmp/' + 'sampling_results.pkl')
-
-print('SMOTETomek')
-smote_tomek = SMOTETomek(random_state=params['random_state'], smote=smote, tomek=tomek)
-x_res, y_res = smote_tomek.fit_sample(data.x, data.y)
-res['smotetomek'] = rf_check_perf(x_res, y_res)
-joblib_dump(x_res, y_res, 'smotetomek')
-del x_res, y_res
+lock = Lock()
+procs = [
+    Process(target=resample, name=name, args=(sampler, name, data.x, data.y, res, lock)) 
+    for name, sampler in samplers.items()
+        ]
+for p in procs:
+    p.start()
+for p in procs:
+    p.join()
 
 joblib.dump(res, 'tmp/' + 'sampling_results.pkl')
 
