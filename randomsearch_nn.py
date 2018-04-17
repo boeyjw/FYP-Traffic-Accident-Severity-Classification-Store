2029ee14a5279260ce86592c937d67545ad6f9fb
boeyjw <psyjwb@nottingham.ac.uk>
2018-04-17 20:21:01 +0100

    checkpoint

--- randomsearch_nn.py
+++ randomsearch_nn.py
@@ -6,64 +6,71 @@ from keras.wrappers.scikit_learn import KerasClassifier
 from keras.layers import Dense, Dropout
 from keras.utils import to_categorical
 from keras.regularizers import l2
+from keras.callbacks import EarlyStopping
 from scipy.stats import randint as sp_randint
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import RandomizedSearchCV, StratifiedKFold
+from sklearn.model_selection import RandomizedSearchCV, StratifiedKFold, train_test_split
 from tap import modelparams
 
 # Reference: http://scikit-learn.org/stable/auto_examples/model_selection/plot_randomized_search.html#sphx-glr-auto-examples-model-selection-plot-randomized-search-py
 # Utility function to report best scores
 def report(results, n_top=3):
     for i in range(1, n_top + 1):
-        candidates = np.flatnonzero(results['rank_test_f1_micro'] == i)
+        candidates = np.flatnonzero(results['rank_test_f1_macro'] == i)
         for candidate in candidates:
             print("Model with rank: {0}".format(i))
             print("Mean validation score: {0:.3f} (std: {1:.3f})".format(
-                results['mean_test_f1_micro'][candidate],
-                results['std_test_f1_micro'][candidate]))
+                results['mean_test_f1_macro'][candidate],
+                results['std_test_f1_macro'][candidate]))
             print("Parameters: {0}".format(results['params'][candidate]))
             print("")
 
-def create_nn(units=125, kernel_reg=0.0001, kernel_init="glorot_uniform", optimizer="adam"):
+def create_nn(units=90, units_2=50, kernel_reg=0.0001, kernel_init="glorot_uniform", optimizer="adam"):
     nn = Sequential()
     # Manually change input_dim to match input shape
-    nn.add(Dense(units=units, input_dim=125, activation="relu", kernel_regularizer=l2(kernel_reg), kernel_initializer=kernel_init))
+    nn.add(Dense(units=units, input_dim=70, activation="relu", kernel_regularizer=l2(kernel_reg), kernel_initializer=kernel_init))
+    nn.add(Dense(units=units_2, activation="relu", kernel_regularizer=l2(kernel_reg), kernel_initializer=kernel_init))
     nn.add(Dense(units=3, activation="softmax"))
     nn.compile(optimizer=optimizer, loss="categorical_crossentropy", metrics=["categorical_accuracy"])
 
     return nn
 if __name__ == "__main__":
     print("Init")
-    ext = ".cas"
+    ext = ".cas.v2"
     params = modelparams.get_constants()
     sample = pd.read_csv("train/stratified_XY_train.oh.tlsmote" + ext + ".csv")
 
-    X = sample.drop("Accident_Severity", axis=1)
+    X = sample.drop("Accident_Severity", axis=1).copy()
     scaler = StandardScaler()
     ordinal_cols = np.array(["Number_of_Casualties", "Number_of_Vehicles", "Speed_Limit", "Age_Band_of_Casualty"])
     ordinal_cols_mask = np.isin(ordinal_cols, X.columns)
     if any(ordinal_cols_mask):
         X[ordinal_cols[ordinal_cols_mask]] = scaler.fit_transform(X[ordinal_cols[ordinal_cols_mask]])
-    Y = sample["Accident_Severity"] - 1
-    del sample
+    Y = sample["Accident_Severity"].copy() - 1
+    Xt, xv, Yt, yv = train_test_split(X, Y, stratify=Y, test_size=0.2)
+    del sample, X, Y
 
-    nn = KerasClassifier(build_fn=create_nn, epochs=50, batch_size=128, verbose=0)
-    n_iter_torun = 20
+    nn = KerasClassifier(build_fn=create_nn, epochs=75, batch_size=258, verbose=0)
+    n_iter_torun = 1
     param_dist = {
-        "units": sp_randint(int(np.round(X.shape[1] * 0.5)),  X.shape[1] + 50),
+        "units": sp_randint(int(np.round(Xt.shape[1] * 0.5)),  Xt.shape[1] + 50),
+        "units_2": sp_randint(int(np.round(Xt.shape[1] * 0.5)),  Xt.shape[1]),
         "kernel_reg": [0.01, 0.001, 0.0001],
         "kernel_init": ["glorot_uniform", "lecun_uniform", "glorot_normal", "lecun_normal"],
         "optimizer": ["adam", "nadam"],
-        "epochs": [75, 100, 150],
-        "batch_size": [128, 256, 512]
+        "batch_size": [128, 256, 512],
+        "epochs": [75, 100, 150]
     }
-    rscv = RandomizedSearchCV(estimator=nn, n_iter=n_iter_torun, param_distributions=param_dist, scoring=["f1_micro", "recall_micro", "precision_micro", "accuracy"], refit="f1_micro", cv=StratifiedKFold(5), verbose=2, return_train_score=False, n_jobs=2, pre_dispatch=2)
+    fit_params = {
+        "callbacks": [EarlyStopping(monitor="val_loss", patience=10)],
+        "validation_data": (xv, to_categorical(yv))
+    }
+    rscv = RandomizedSearchCV(estimator=nn, n_iter=n_iter_torun, param_distributions=param_dist, scoring=["f1_macro", "recall_macro", "precision_macro", "accuracy"], refit="f1_macro", cv=StratifiedKFold(5), verbose=2, return_train_score=False, n_jobs=2, pre_dispatch=2, fit_params=fit_params)
 
-    print("Search RF: " + ext)
-    rscv.fit(X, Y)
+    print("Search NN: " + ext)
+    rscv.fit(Xt, Yt)
     cv_res = pd.DataFrame(rscv.cv_results_)
     print("Dump")
     cv_res.to_csv("search/nn/randomsearch_nn" + ext + ".csv", index=False)
-    rscv.best_estimator_.model.save("search/nn/" + "best_nn" + ext + ".randomsearch.h5")
     report(rscv.cv_results_, n_top=n_iter_torun)
 
--- randomsearch_rf.py
+++ randomsearch_rf.py
@@ -1,8 +1,8 @@
 import numpy as np
+import pandas as pd
 
 from scipy.stats import randint as sp_randint
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.externals import joblib
 from sklearn.model_selection import RandomizedSearchCV, StratifiedKFold
 from tap import modelparams
 
@@ -10,32 +10,37 @@ from tap import modelparams
 # Utility function to report best scores
 def report(results, n_top=3):
     for i in range(1, n_top + 1):
-        candidates = np.flatnonzero(results['rank_test_score'] == i)
+        candidates = np.flatnonzero(results['rank_test_f1_macro'] == i)
         for candidate in candidates:
             print("Model with rank: {0}".format(i))
             print("Mean validation score: {0:.3f} (std: {1:.3f})".format(
-                results['mean_test_score'][candidate],
-                results['std_test_score'][candidate]))
+                results['mean_test_f1_macro'][candidate],
+                results['std_test_f1_macro'][candidate]))
             print("Parameters: {0}".format(results['params'][candidate]))
             print("")
 
-print("Init")
-params = modelparams.get_constants()
-rf = RandomForestClassifier(n_estimators=50, random_state=params["RANDOM_STATE"], oob_score=True, n_jobs=params["N_JOBS"])
-n_iter_torun = 100
-ext = ".nocas"
-param_dist = {
-    "max_depth": [None] + list(range(3, 50, 2)),
-    "min_samples_leaf": sp_randint(1, 100),
-    "n_estimators": sp_randint(50, 150),
-    "max_features": ["sqrt", "log2"]
-}
-rscv = RandomizedSearchCV(estimator=rf, n_iter=n_iter_torun, param_distributions=param_dist, scoring="f1_micro", cv=StratifiedKFold(5), verbose=1)
-
 if __name__ == "__main__":
-    sample = joblib.load("train/stratified_XY_train.oh.tlsmote" + ext + ".pkl.xz")
+    print("Init")
+    params = modelparams.get_constants()
+    rf = RandomForestClassifier(n_estimators=50, random_state=params["RANDOM_STATE"], oob_score=True, n_jobs=params["N_JOBS"])
+    n_iter_torun = 20
+    ext = ".cas.v2"
+    param_dist = {
+        "max_depth": [None] + list(range(3, 50, 2)),
+        "min_samples_leaf": sp_randint(1, 100),
+        "n_estimators": sp_randint(50, 150),
+        "max_features": ["sqrt", "log2"]
+    }
+    rscv = RandomizedSearchCV(estimator=rf, n_iter=n_iter_torun, param_distributions=param_dist, scoring=["f1_macro", "precision_macro", "recall_macro", "accuracy"], refit="f1_macro", cv=StratifiedKFold(5), verbose=2, return_train_score=False)
+
+    sample = pd.read_csv("train/stratified_XY_train.oh.tlsmote" + ext + ".csv")
+    X = sample.drop("Accident_Severity", axis=1).copy()
+    Y = sample["Accident_Severity"].copy()
+    del sample
 
     print("Search RF")
-    rscv.fit(sample["X"], sample["Y"])
-    joblib.dump(rscv, "random_search_rf" + ext + ".pkl.xz")
-    report(rscv.cv_results_)
+    rscv.fit(X, Y)
+    print("Dump")
+    res = pd.DataFrame(rscv.cv_results_)
+    res.to_csv("search/rf/randomsearch_rf" + ext + ".csv", index=False)
+    report(rscv.cv_results_, n_top=n_iter_torun)
